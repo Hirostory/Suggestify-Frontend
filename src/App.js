139e5bb9261e46756a3e9a93af1e8c6c708c9eeb
nehaleloraby <nehal.eloraby17@gmail.com>
2023-10-16 22:27:01 -0400

    auth works & new folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import Account from './pages/Account'
+
+
+
 function App() {
+  const isUserSignedIn = !!localStorage.getItem('token')
   return (
     <div className="App">
-      
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        {isUserSignedIn && <Route path='/account' element={<Account />} /> }
+      </Routes>
     </div>
-  );
+  )
+  
 }
 
-export default App;
+export default App
+
---
+++ src/pages/Login.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import './Login.css'
+
+function Login() {
+  const [users, setUsers] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  const fetchUsers = () => {
+    axios.get('http://localhost:3000/user/register')
+      .then((res) => {
+        console.log(res.data)
+      })
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const response = await axios.post('http://localhost:3000/user/login', { username, password })
+      const token = response.data.token
+      alert('Login successful')
+      setUsername('')
+      setPassword('')
+      fetchUsers()
+      navigate('account')
+      window.location.reload()
+      localStorage.setItem('token', token)
+    } catch (error) {
+      console.log('Login Error')
+    }
+  }
+
+  return (
+    <div className="login-container">
+      <div className="login-header">
+        <h2>LOGIN</h2>
+      </div>
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label>Username</label>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="center-button">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login
